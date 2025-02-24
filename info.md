448d748187dc3b356a59558762c73e28f035c29d
EnricoCiccolini <piroroma90@gmail.com>
2025-02-24 15:30:13 +0100

    feat : add javascript code

---
+++ info.md
@@ -0,0 +1,13 @@
+# java biglietto del treno 
+
+## esecuzione 
+
+- per prima cosa creiamo le due variabili ( age e kilometer) e una variabile sconto a cui assegnamo 0
+- chiediamo all' utente di dirci l eta e assegnamo il volore ad age
+- chiediamo all' utente di dirci i km da percorrere e assegnamo il valore a kilometer 
+- controlliamo che tutti e due le variabili siano numeriche 
+     - se non lo sono diciamo che ci occorrono delle informazioni corrette 
+     - se lo sono controlliamo l eta 
+        - se l eta è minore di 18  assegnamo alla variaile sconto 20
+        - se l eta è magiore di 60 assegnamo alla variabile sconto 40
+    ora mostriamo a schermo : il prezzo è ( kilometer -( ( kilometer * sconto ) /100))
---
+++ script.js
@@ -0,0 +1,40 @@
+// dichiarazione variabili
+let age
+let kilometer
+let disocount = 0
+let price
+
+// assegnazioni dei valori dell utente 
+
+age = prompt ("Ciao inserisci la tua età")
+kilometer = prompt ("Ora dimmi i kilometri che vorrai percorrere")
+
+
+// conversione da stringa a numeri  
+age = parseInt(age)
+kilometer = parseInt(kilometer)
+
+// controlli 
+
+if (age === NaN || kilometer === NaN){
+    console.log -( "età o kilometri non validi per favore inserire un numero valido")
+
+    // controlli eta per lo sconto 
+} else {
+    if (age <= 18){
+        disocount = 20
+    }
+    else if (age >= 60) {
+        disocount = 40
+    }
+    // calcolo sconto 
+
+    disocount = (0.21 * disocount)/100
+
+    price = (kilometer * 0.21 ) - disocount
+    price = price.toFixed(2)
+
+    // dichiarazione prezzo totale 
+   console.log (`il totale del tuo viaggio  ${ price} euro`) 
+}
+
